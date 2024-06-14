e9b9f9fe67a3ad083a5d664300910ef564587ebf
alelliott <alexjmelliott@gmail.com>
2024-06-14 13:39:20 -0400

    static chart value updated

--- src/common/InfoDashboard/helpers.ts
+++ src/common/InfoDashboard/helpers.ts
@@ -5,7 +5,7 @@ import { formatEther } from '@/utils'
 type ChartData = Record<number, BigNumber>
 
 export async function getChartData(): Promise<ChartData> {
-  const totalDeposited = BigNumber.from("272000000000000000000") // Static value
+  const totalDeposited = BigNumber.from('450000000000000000000') // Static value
   const today = new Date().setHours(0, 0, 0, 0) // Start of today in milliseconds
 
   const data: ChartData = {
