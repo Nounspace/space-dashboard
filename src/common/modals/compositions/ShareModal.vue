1e55b3895ee50af713b9353acfadb7aae4e2291f
Jesse Paterson <jesse.r.pat@gmail.com>
2024-06-16 13:04:28 -0400

    testing intent link buttons

--- src/common/modals/compositions/ShareModal.vue
+++ src/common/modals/compositions/ShareModal.vue
@@ -11,6 +11,24 @@
       <div id="share-modal" class="modal">
         <div class="modal-content">
             <div class="share-modal__buttons-wrp">
+              <a href="https://warpcast.com/~/compose?
+              text=Hello%20@farcaster!
+              &embeds[]=https://farcaster.xyz
+              &embeds[]=https://github.com/farcasterxyz/protocol">
+                <app-button
+                  class="share-modal__buttons-wrp"
+                  :text="$t('home-page.public-pool-view.cast-btn')"
+                />
+              </a>
+              <a href="https://twitter.com/intent/tweet?
+              text=I%20just%20staked%20%24%7BethAmount%7D%20stETH%20in%20the%20Nounspace%20Fair%20Launch%21%20Total%20staked%3A%20%24%7BtotalStaked%7D%20stETH.%20Connect%20with%20@NounspaceTom%20and%20@nounspace%21">
+                <app-button
+                  class="share-modal__buttons-wrp"
+                  :text="$t('home-page.public-pool-view.tweet-btn')"
+                /> 
+              </a>
+            </div>
+            <div>
               <p>Connect with us:</p>
               <a href="https://twitter.com/NounspaceTom" target="_blank">@NounspaceTom </a>
               <a href="https://twitter.com/nounspace" target="_blank">@nounspace</a>
@@ -52,6 +70,18 @@ withDefaults(
 
   @include respond-to(medium) {
     margin-top: toRem(36);
+  }
+}
+
+.share-modal__button {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  gap: toRem(16);
+
+  @include respond-to(medium) {
+    flex-direction: column;
+    gap: toRem(8);
   }
 }
 
