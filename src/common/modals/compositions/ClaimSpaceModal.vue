01e6a109c4639d87e76355b1070d23deb131a129
Willy Ogorzaly <willy@ogorzaly.com>
2024-10-31 14:42:04 -0500

    Updating claim portal
    Claim portal for space drop #1: https://app.hedgey.finance/claim/11f1e34c-1dc8-49b6-bd48-4008842a4a81

--- src/common/modals/compositions/ClaimSpaceModal.vue
+++ src/common/modals/compositions/ClaimSpaceModal.vue
@@ -5,7 +5,7 @@
     @update:is-shown="emit('update:is-shown', $event)"
   >
     <template #default="{ modal }">
-      <iframe src="https://app.hedgey.finance/claim/1da6c50a-badd-44d2-a005-00a89409ead6" frameborder="0" class="full-size-iframe"></iframe>
+      <iframe src="https://app.hedgey.finance/claim/11f1e34c-1dc8-49b6-bd48-4008842a4a81" frameborder="0" class="full-size-iframe"></iframe>
     </template>
   </iframe-modal>
 </template>
